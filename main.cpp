291b560d97f93a7d4f116e9c5d4b990bb0222c54
icmp <jmfield2@mail.usf.edu>
2013-10-24 10:58:36 -0400

    part 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <iomanip>
 
 #include <iostream>
 #include <fstream>
@@ -12,6 +13,7 @@
 
 using namespace std;
 
+#define DEBUG 1
 
 //GLOBAL VARIABLES
 const string inputFile = "input.txt";
@@ -104,6 +106,7 @@ int GetInput(vector<day>& daysList)
 
 void CalculateTables(vector<day> &daysList, vector< vector<int> > &Costs, vector< vector<bool> > &Choice, int numDays, int &numReboots)
 {
+
     //Fill in table for last column.  We logically will never choose to reboot on the last day.
     for(int i = 0; i < numDays; i++)
     {
@@ -115,16 +118,26 @@ void CalculateTables(vector<day> &daysList, vector< vector<int> > &Costs, vector
         Costs[lastDay][i] = minCost;
         Choice[lastDay][i] = 1; //no reason to reboot on last day.
     }
-    //Fill in table for rest of columns.
+
+    // Fill in table for rest of columns.
+    //
+    // Table will have the SUM cost of i+(i+1) for a given column
     for(int i = (numDays-2); i >= 0; i--)
     {
+
+	// Starting from day before last day, iterate each day from last to first
+	// Then, iterate from 0th day up to ith day since reboot
         for(int j = 0; j <= i; j++)
         {
+	    // Set left-hand to yesterday's max (0th day from reboot)
             int lhs = Costs[i+1][0];
+
+	    // Set right-hand to min(available, able) + yesterday sum (i+1 day, j+1 since reboot ... the diagonal)
             int rhs = min(daysList[i].dataAvailable, daysList[j].dataAbility) + Costs[i+1][j+1] ;
             int maximum = max(lhs, rhs);
+
             Costs[i][j] = maximum;
-            
+
             if(maximum == lhs)
             {
                 Choice[i][j] = 0; //reboot
@@ -135,25 +148,53 @@ void CalculateTables(vector<day> &daysList, vector< vector<int> > &Costs, vector
             {
                 Choice[i][j] = 1; //process data
             }
-            
+ 
         }
+
     }
+
+#ifdef DEBUG
+for (int i=0; i < numDays; i++) {
+	for (int j=0; j < numDays; j++) 
+		cout << setw(5) << Costs[j][i] << "-" << Choice[j][i] << " ";
+	cout << endl;
+}
+#endif
+
 }
 
 void WriteOutput(vector< vector<int> > &Costs, vector< vector<bool> > &Choice, int numReboots, int numDays)
 {
+    vector<int> rebootDays;
+
     ofstream outFile;
     outFile.open(outputFile.data(), ios::app);
     if(outFile.is_open())
     {
-        outFile << Costs[0][0] << endl << numReboots << endl;
+	/* 
+	 Traverse CHOICE n*n vector in a diagonal looking for
+	 the days we reboot...if found, go back to 0 and continue
+	 */
+
+        int j = 0;
+        outFile << Costs[0][0] << endl;
         for(int i = 0; i < numDays; i++)
         {
-            if(Choice[i][0] == 0)
+            if(Choice[i][j] == 0)
             {
-                outFile << (i+1) << " ";
+                rebootDays.push_back(i+1);
+                j = 0;
             }
+            else{j++;}
         }
+
+        outFile << rebootDays.size() << endl;
+
+        for(int i = 0; i < rebootDays.size(); i++)
+        {
+            outFile << rebootDays[i] << " ";
+        }
+
         outFile << "\n\n";
     }
     outFile.close();
